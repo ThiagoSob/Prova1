35a74202263c937753220eb464b9f0979b4c3a31
ThiagoSob <thiago.sobreira1990@outlook.com>
2024-05-05 17:38:59 -0300

    Add files via upload

---
+++ Prova1.py
@@ -0,0 +1,72 @@
+#Tipos de Variáveis
+#Crie um exemplo de cada uma das principais tipagens de variáveis
+
+print('----'*10)
+
+print(f'\n\nProva 1\n\n')
+
+print('----'*10)
+
+print(f'\n\nThiago Sobreira\n\n')
+
+print('----'*10)
+
+print("\n\nQuestão 1:\nCrie um exemplo de cada uma das principais tipagens de variáveis\n\n")
+
+print('----'*10)
+
+n = 1
+n1 = 2.3
+n2 = "Nome"
+n3 = True
+
+print("\n\n")
+
+print(type(n))
+print("\n\n")
+print(type(n1))
+print("\n\n")
+print(type(n2))
+print("\n\n")
+print(type(n3))
+print('\n\n')
+
+print('----'*10)
+
+#Crie uma solicitação de dados ao usuário.
+#Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+
+print(f"\n\nQuestões 2 e 3:\nCrie uma solicitação de dados ao usuário.\nImprima a solicitação anterior na tela com uma mensagem personalizada.\n\n")
+
+print('----'*10)
+
+print(f'\n\nBANCO DE DADOS DO VEÍCULO:\n\n')
+
+print('----'*10)
+
+print(f"\n\nInsira os dados do seu veículo:")
+
+
+p = input(f'\n\nNome do Proprietário: ')
+t = input(f"\n\nTipo do Veículo: ")
+a = int(input(f'\n\nAno de Fabricação: '))
+pt = float(input(f'\n\nCilindradas: '))
+
+print("\n\n")
+
+print('----'*10)
+
+print(f"\n\nParabéns!\nProprietário {p}, seu veículo foi cadastrado com sucesso!\n\nSegue as informações cadastradas:\n\n")
+
+print('----'*10)
+
+print(f"\n\nTipo: {t}")
+print(f"\nAno de Fabricação: {a}")
+print(f"\nCinlidradas: {pt}\n\n")
+
+print('----'*10)
+
+print("\n\nFIM\n\n")
+
+print('----'*10)
